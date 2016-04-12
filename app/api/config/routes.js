a79f9a561deee91e3308138253cdd8f8c2cb5945
Linus Norton <linusnorton@gmail.com>
2016-04-12 21:23:46 +0100

    removed generated js file

--- app/api/config/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var UserController_1 = require('../src/controller/UserController');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    "/user": {
-        "get": UserController_1.default.get
-    }
-};
